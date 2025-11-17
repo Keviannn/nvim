9553ec252c7d2dbc85f0b211810820da132d3add
kevian <joseluisprasie@gmail.com>
2025-11-17 16:22:22 +0100

    Auto apply treesitter styles

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",  -- para actualizar los parsers
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            highlight = {
+                enable = true,                   -- activar resaltado
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    end,
 }
