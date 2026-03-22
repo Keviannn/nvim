b51abdf67e0ea7cbf3577a7272cd8bf4d5e8637d
Kevian <joseluisprasie@gmail.com>
2026-03-22 01:41:28 +0100

    Fixed word config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",      -- Actualiza lenguajes / Update languajes
     config = function()
-        require'nvim-treesitter.configs'.setup {
+        require('nvim-treesitter.config').setup {
             highlight = {
                 enable = true,     -- Activa los colores de treesitter / Enables treesitter coloring
                 additional_vim_regex_highlighting = false,
